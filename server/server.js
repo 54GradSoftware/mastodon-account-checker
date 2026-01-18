405020527695c53d899b7dc9b1bca4d041721ab6
scammo <sbrinkmann@54gradsoftware.de>
2026-01-19 00:27:32 +0100

    🖥️ add hosting

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import rateLimit from 'express-rate-limit';
 import { calculateMastodonAccountScore } from './index.js';
 
 const app = express();
-const PORT = process.env.PORT || 1;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
